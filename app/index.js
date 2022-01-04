e8c949745827bc669a40cf0923c6e8f04b432ab5
Karashchuk_M <myranika@gmail.com>
2022-01-04 04:46:49 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+function render() {
+    const productsStore = localStorageUtil.getProducts();
+
+    headerPage.render(productsStore.length);
+    productsPage.render();
+}
+
+spinnerPage.render();
+
+let CATALOG = [];
+
+fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+        CATALOG = body;
+
+        setTimeout(() => {
+            spinnerPage.handleClear();
+            render();
+        });
+    })
+    .catch(error => {
+        spinnerPage.handleClear();
+        //console.log(error);
+        errorPage.render();
+    });
+//axios
